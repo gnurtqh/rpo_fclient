15ba4f88c926544c4f597f8347750cf10ae08830
trunqh <hqtxxx@gmail.com>
2021-03-08 01:26:08 +0300

    done 2 lab

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,15 +1,22 @@
 package ru.iu3.fclient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
     }
 
     @Override
@@ -17,14 +24,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Button btn = findViewById(R.id.btnClickMe);
+        btn.setOnClickListener((View v) -> { onButtonClick(v);});
+
+        int res = initRng();
+        Log.i("fclient", "Init Rng = " + res);
+        byte[] rnd = randomBytes(16);
+        byte[] data = {'a', 'b', 'c', 'd', '1', '2', '3', '4'};
+        byte[] encrypted = encrypt(rnd, data);
+        byte[] decrypted = decrypt(rnd, encrypted);
+        String originalData = new String(data, StandardCharsets.UTF_8);
+        String encryptedData = new String(encrypted, StandardCharsets.UTF_8);
+        String decryptedData = new String(decrypted, StandardCharsets.UTF_8);
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    public static byte[] StringToHex(String s) {
+        try {
+            return Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex) {
+
+        }
+        return new byte[0];
+    }
+
+    protected void onButtonClick(View v) {
+        Toast.makeText(this, stringFromJNI(), Toast.LENGTH_SHORT).show();
+    }
+
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
 }
