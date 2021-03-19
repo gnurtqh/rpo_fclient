178a013b723ae1dafc48e78d09110d8e07fca0e4
trunqh <hqtxxx@gmail.com>
2021-03-19 11:45:17 +0300

    feat(backend): init backend spring boot

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -9,8 +9,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,18 +32,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.btnClickMe);
-        btn.setOnClickListener((View v) -> { onButtonClick(v);});
+        btn.setOnClickListener((View v) -> {
+            onButtonClick(v);
+        });
+
+        Button btnTestHttp = findViewById(R.id.buttonTestHttp);
+        btnTestHttp.setOnClickListener((View v) -> {
+            onButtonTestHttpClick(v);
+        });
 
         int res = initRng();
         Log.i("fclient", "Init Rng = " + res);
-//        byte[] rnd = randomBytes(16);
-//        byte[] data = {'a', 'b', 'c', 'd', '1', '2', '3', '4'};
-//        byte[] encrypted = encrypt(rnd, data);
-//        byte[] decrypted = decrypt(rnd, encrypted);
-//        String originalData = new String(data, StandardCharsets.UTF_8);
-//        String encryptedData = new String(encrypted, StandardCharsets.UTF_8);
-//        String decryptedData = new String(decrypted, StandardCharsets.UTF_8);
-
     }
 
     public static byte[] StringToHex(String s) {
@@ -52,32 +57,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onButtonClick(View v) {
-//        Toast.makeText(this, stringFromJNI(), Toast.LENGTH_SHORT).show();
-//        byte[] key = randomBytes(16);
-//        byte[] data = {5, 3, 4, 7, 8, 9, 3};
-//        System.out.println(data);
-//        byte[] enc = encrypt(key, data);
-//        byte[] dec = decrypt(key, enc);
-//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
-//        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
         Intent it = new Intent(this, PinPadActivity.class);
         startActivityForResult(it, 0);
     }
-//    protected void TestHttpCLient(){
-//        new Thread(()->{
-//            try {
-//
-//            } catch (Exception ex) {
-//                Log.e("fapptag", "Http Client Fails", ex);
+    protected void onButtonTestHttpClick(View v) {
+        TestHttpClient();
+    }
+    protected void TestHttpClient() {
+        new Thread(() -> {
+            try {
+//              HttpURLConnection uc = (HttpURLConnection) (new URL("https://www.wikipedia.org").openConnection());
+                HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() -> {
+                    Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+
+
+    protected String getPageTitle(String html){
+//        int pos = html.indexOf("<title");
+//        String p = "not found";
+//        if(pos >=0){
+//            int pos2 = html.indexOf("<", pos+1);
+//            if(pos2>=0){
+//                p = html.substring(pos+7, pos2);
 //            }
-//        }).start();
-//    }
-
-
+//        }
+//        return p;
+        Pattern partern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = partern.matcher(html);
+        String p;
+        if(matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
+
 
 }
